387a2492d59d61c3af0d2fdd50857db65c4a8d2e
jan <janpawlak.pl@gmail.com>
2025-12-22 00:18:30 +0100

    Added basic graphing

--- src/apps/calc.h
+++ src/apps/calc.h
@@ -26,6 +26,85 @@ void updateCalc() {
 
 }
 
+void displayGraph(double top, double bottom, double left, double right) {
+    display.fillScreen(ILI9341_BLACK);
+
+    double Ydiff = top - bottom, Xdiff = right - left;
+
+    if(top > 0 && bottom < 0) {
+        double pos = top / Ydiff;
+        int y = pos * 240;
+        display.drawLine(0, y, 320, y, ILI9341_WHITE);
+    }
+
+    if(left < 0 && right > 0) {
+        double pos = right / Xdiff;
+        int x = pos * 320;
+        display.drawLine(x, 0, x, 240, ILI9341_WHITE);
+    }
+
+    double x, step = (right - left) / 320.0;
+    te_variable vars[] = {{"x", &x}};
+    te_expr *expr = te_compile(textInput.c_str(), vars, 1, 0);
+
+    for(x = (left + step/2); x < (right - step/2); x += step) {
+        double y = te_eval(expr);
+        double X = (x - left) / Xdiff, Y = (y - bottom) / Ydiff;
+        int realX = (1.00-X) * 320, realY = (1.00-Y) * 240;
+        display.drawPixel(realX, realY, ILI9341_CYAN);
+    }
+    
+    te_free(expr);
+}
+
+void graph(double top, double bottom, double left, double right) {
+    while(true) {
+        double scale = (top-bottom) / 240;
+        if(!SystemEventQueue.empty()) {
+            auto front = SystemEventQueue.front();
+            if(front.type == false) {
+                switch (front.button) {
+                    case 17: // exit
+                        return;
+                        break;
+                    case 4: // up
+                        top += scale * 24;
+                        bottom += scale * 24;
+                        break;
+                    case 9: // left
+                        left += scale * 32;
+                        right += scale * 32;
+                        break;
+                    case 11: // right
+                        left -= scale * 32;
+                        right -= scale * 32;
+                        break;
+                    case 16: // down
+                        top -= scale * 24;
+                        bottom -= scale * 24;
+                        break;
+                    case 34: // zoom in
+                        top /= 10;
+                        bottom /= 10; 
+                        left /= 10;
+                        right /= 10;
+                        break;
+                    case 35: // zoom out
+                        top *= 10;
+                        bottom *= 10; 
+                        left *= 10;
+                        right *= 10;
+                        break;
+                }
+                
+                displayGraph(top, bottom, left, right);
+            }
+        }
+        SystemEventQueue.pop();
+        delay(30);
+    }
+}
+
 void calc() {
     updateCalc();
     while(true) {
@@ -59,7 +138,7 @@ void calc() {
                         updateCalc();
                         break;
 
-                    case 10:
+                    case 10: // calculate
                         display.setCursor(10, 215);
                         display.print(String("= ") + String(te_interp(textInput.c_str(), 0)));
                         break;
@@ -70,6 +149,10 @@ void calc() {
 
                     case 1:
                         layer = 1;
+                        break;
+                    
+                    case 7:
+                        graph(1, -1, -1, 1);
                         break;
 
                     default:
